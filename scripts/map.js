68f22096e1d6639e90884e4653b296f924f10da9
Anton Khleb <toni.farsh@icloud.com>
2025-05-03 16:19:19 +0500

    fix

--- scripts/map.js
+++ scripts/map.js
@@ -1,28 +1,44 @@
 initMap();
 
 async function initMap() {
-    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
     await ymaps3.ready;
 
-    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+    const {
+        YMap,
+        YMapDefaultSchemeLayer,
+        YMapDefaultFeaturesLayer,
+        YMapMarker,
+    } = ymaps3;
 
-    // Иницилиазируем карту
-    const map = new YMap(
-        // Передаём ссылку на HTMLElement контейнера
-        document.getElementById('map'),
+    const map = new YMap(document.getElementById('map'), {
+        location: {
+            center: [43.8346 ,43.8414 ],
+            zoom: 10
+        },
+       
+    });
 
-        // Передаём параметры инициализации карты
-        {
-            location: {
-                // Координаты центра карты
-                center: [37.588144, 55.733842],
-
-                // Уровень масштабирования
-                zoom: 10
-            }
-        }
-    );
-
-    // Добавляем слой для отображения схематической карты
     map.addChild(new YMapDefaultSchemeLayer());
+    map.addChild(new YMapDefaultFeaturesLayer());
+
+    const cities = [
+        { name: 'Новопавловск', coords: [ 43.631910 ,43.957369] },
+        { name: 'Прохладный', coords: [44.009983,  43.759045] },
+        { name: 'Алтуд', coords: [43.870300,  43.720871] }
+    ];
+
+    for (const city of cities) {
+        const markerElement = document.createElement('div');
+        markerElement.classList.add('map-marker');
+        markerElement.innerHTML = `
+        <span class="name">${city.name}</span>
+        `
+        markerElement.title = city.name;
+
+        const marker = new YMapMarker({ coordinates: city.coords }, markerElement);
+        map.addChild(marker);
+    }
+
+
+
 }
