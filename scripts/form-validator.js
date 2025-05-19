d45a9ebccd2208fda53259bedfc1b464cf30fbbc
Anton Khleb <khleb.4nton@yandex.ru>
2025-05-19 15:53:15 +0500

    fix

--- scripts/form-validator.js
+++ scripts/form-validator.js
@@ -1,43 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('form').forEach(function(form) {
-        const submitBtn = form.querySelector('button[type="submit"]');
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelectorAll('form').forEach(function(form) {
+//         const submitBtn = form.querySelector('button[type="submit"]');
         
-        function checkForm() {
-            let isFormValid = true;
+//         function checkForm() {
+//             let isFormValid = true;
             
-            form.querySelectorAll('input[required], select[required]').forEach(function(input) {
+//             form.querySelectorAll('input[required], select[required]').forEach(function(input) {
 
-                if (input.classList.contains('original-select')) {
-                    if (!input.value) isFormValid = false;
-                } 
-                // Для обычных input-полей
-                else if (!input.value.trim()) {
-                    isFormValid = false;
-                }
-            });
+//                 if (input.classList.contains('original-select')) {
+//                     if (!input.value) isFormValid = false;
+//                 } 
+//                 // Для обычных input-полей
+//                 else if (!input.value.trim()) {
+//                     isFormValid = false;
+//                 }
+//             });
             
-            if (isFormValid) {
-                submitBtn.classList.remove('disabled');
-                submitBtn.disabled = false;
-            } else {
-                submitBtn.classList.add('disabled');
-                submitBtn.disabled = true;
-            }
-        }
+//             if (isFormValid) {
+//                 submitBtn.classList.remove('disabled');
+//                 submitBtn.disabled = false;
+//             } else {
+//                 submitBtn.classList.add('disabled');
+//                 submitBtn.disabled = true;
+//             }
+//         }
         
-        checkForm();
+//         checkForm();
         
-        form.querySelectorAll('input[required], select[required]').forEach(function(input) {
-            if (input.tagName === 'INPUT') {
-                input.addEventListener('input', checkForm);
-            }
-            if (input.tagName === 'SELECT') {
-                input.addEventListener('change', checkForm);
-            }
-        });
+//         form.querySelectorAll('input[required], select[required]').forEach(function(input) {
+//             if (input.tagName === 'INPUT') {
+//                 input.addEventListener('input', checkForm);
+//             }
+//             if (input.tagName === 'SELECT') {
+//                 input.addEventListener('change', checkForm);
+//             }
+//         });
         
-        form.querySelectorAll('.custom-option').forEach(function(option) {
-            option.addEventListener('click', checkForm);
-        });
-    });
-});
+//         form.querySelectorAll('.custom-option').forEach(function(option) {
+//             option.addEventListener('click', checkForm);
+//         });
+//     });
+// });
